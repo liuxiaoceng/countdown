8fc5ce2aab6bea0d831de1a582b19f93feeb0f1d
liuxiaoceng <liuxiaoceng@hotmail.com>
2016-02-23 18:57:47 +0800

    add screen adaptation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;		
-var RADIUS = 8;
-var MARGIN_TOP = 60;
-var MARGIN_LEFT = 30;
+var WINDOW_WIDTH = document.body.clientWidth;
+var WINDOW_HEIGHT = document.body.clientHeight;
+var MARGIN_LEFT = Math.round(WINDOW_WIDTH/10);	// 两边占 1/5
+var MARGIN_TOP = Math.round(WINDOW_HEIGHT/5);	
+var RADIUS =  Math.round(WINDOW_WIDTH*4/5/108)-1;	// 数字内容占 4/5
 
 // 设置截止时间
 const endTime = new Date(2016, 2-1, 26, 13, 36, 42); 
@@ -13,177 +13,178 @@ var balls = [];
 // 颜色数组 - 随机此颜色来设置小球
 const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"];
 
-window.onload = function() {
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext('2d');
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext('2d');
 
-	if (context) {
-		canvas.width = WINDOW_WIDTH;
-		canvas.height = WINDOW_HEIGHT;
-		
-		curShowTimeSeconds = getCurrentShowtimeSeconds();
-		setInterval(function(){
-			render(context);
-			update();
-		}, 50);
-		
-		
-	} else {
-			alert("当前浏览器不支持canvas, 请更换浏览器再试")
-	}
+if (context) {
 
-	function getCurrentShowtimeSeconds() {
-		var curTime = new Date();
-		var ret = endTime.getTime() - curTime.getTime();
-		ret = Math.round(ret/1000);
+	canvas.width = WINDOW_WIDTH;
+	canvas.height = WINDOW_HEIGHT;
 
-		return ret >=0? ret: 0;
-	}
-
-	// 渲染整个界面
-	function render(cxt) {
-		// clear last images
-		cxt.clearRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-		var hours = parseInt(curShowTimeSeconds/3600);
-		var minutes = parseInt(curShowTimeSeconds%3600/60);
-		var seconds = parseInt(curShowTimeSeconds%60);
-		var itemSize = RADIUS+1;
-
-		var num = 0;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(hours/10), cxt);
-		num += 7*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(hours%10), cxt);
-		num += 7*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, 10, cxt);
 	
-		num += 4*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(minutes/10), cxt);
-		num += 7*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(minutes%10), cxt);
-		
-		num += 7*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, 10, cxt);
-		num += 4*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(seconds/10), cxt);
-		num += 7*2+1;
-		renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(seconds%10), cxt);
+	curShowTimeSeconds = getCurrentShowtimeSeconds();
+	setInterval(function(){
+		render(context);
+		update();
+	}, 50);
+	
+	
+} else {
+		alert("当前浏览器不支持canvas, 请更换浏览器再试")
+}
 
-		/*
-			绘制小球
-		*/
-		for (var i = 0; i < balls.length; i++) {
-			cxt.fillStyle = balls[i].color;
+function getCurrentShowtimeSeconds() {
+	var curTime = new Date();
+	var ret = endTime.getTime() - curTime.getTime();
+	ret = Math.round(ret/1000);
 
-			cxt.beginPath();
-			cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2*Math.PI, true);
-			cxt.closePath();
+	return ret >=0? ret: 0;
+}
 
-			cxt.fill();
-		}
-	}
-	// 渲染单个数字
-	function renderDigit(x, y, num, cxt) {
-		cxt.fillStyle = "rgb(0,102,153)";
+// 渲染整个界面
+function render(cxt) {
+	// clear last images
+	cxt.clearRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-		for (var i = 0; i < digit[num].length; i++) {
-			for (var j = 0; j < digit[num][i].length; j++) {
-				if (digit[num][i][j] == 1) {
-					cxt.beginPath();
-					cxt.arc( x+j*2*(RADIUS+1)+(RADIUS+1), y+i*2*(RADIUS+1)+(RADIUS+1), RADIUS, 0, 2*Math.PI );
-					cxt.closePath();
+	var hours = parseInt(curShowTimeSeconds/3600);
+	var minutes = parseInt(curShowTimeSeconds%3600/60);
+	var seconds = parseInt(curShowTimeSeconds%60);
+	var itemSize = RADIUS+1;
 
-					cxt.fill();
-				}
-			}
-		}
-	}
+	var num = 0;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(hours/10), cxt);
+	num += 7*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(hours%10), cxt);
+	num += 7*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, 10, cxt);
 
-	// 更新球的位置，速度等信息
-	function updateBalls() {
-		for (var i = 0; i < balls.length; i++) {
-				balls[i].x += balls[i].vx;
-				balls[i].y += balls[i].vy;
-				balls[i].vy += balls[i].g;
+	num += 4*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(minutes/10), cxt);
+	num += 7*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(minutes%10), cxt);
+	
+	num += 7*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, 10, cxt);
+	num += 4*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(seconds/10), cxt);
+	num += 7*2+1;
+	renderDigit( MARGIN_LEFT + num*itemSize, MARGIN_TOP, parseInt(seconds%10), cxt);
 
-				// 下单元碰撞检测
-				if (balls[i].y >= (WINDOW_HEIGHT - RADIUS)) {
-					balls[i].y = WINDOW_HEIGHT - RADIUS;	// 着地
-					balls[i].vy = -balls[i].vy*0.75;	// 增加摩擦系数，速度逐渐降低
-				}
-		}
+	/*
+		绘制小球
+	*/
+	for (var i = 0; i < balls.length; i++) {
+		cxt.fillStyle = balls[i].color;
 
-		var cnt = 0;
-		for (var i = 0; i < balls.length; i++) {
-			if (balls[i].x+RADIUS > 0 && balls[i].x-RADIUS < WINDOW_WIDTH) 
-				balls[cnt++] = balls[i];
-		}
+		cxt.beginPath();
+		cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2*Math.PI, true);
+		cxt.closePath();
 
-		while(balls.length > cnt) {
-			balls.pop();
-		}
-		
-	}
-	// 为对应的数字添加相应数量的球
-	function addBalls(x, y, num) {
-		for (var i = 0; i < digit[num].length; i++) {
-			for (var j = 0; j < digit[num][i].length; j++) {
-				if (digit[num][i][j] == 1) {
-					var aBall = {
-						x: x+j*2*(RADIUS+1)+(RADIUS+1),
-						y: y+i*2*(RADIUS+1)+(RADIUS+1),
-						g: 1.5+Math.random(),
-						vx: Math.pow(-1, Math.ceil(Math.random()*1000))*4,
-						vy: -5,
-						color: colors[Math.floor(Math.random()*colors.length)]
-					}
-					balls.push(aBall);
-				} // end if
-			} // enf for
-		} // end for
-	}
-	// 更新时间和球的信息
-	function update() {
-		var nextShowTimeSeconds = getCurrentShowtimeSeconds();
-		var nextHours = parseInt(nextShowTimeSeconds/3600);
-		var nextMinutes = parseInt(nextShowTimeSeconds%3600/60);
-		var nextSeconds = parseInt(nextShowTimeSeconds%60);	
-
-		var curHours = parseInt(curShowTimeSeconds/3600);
-		var curMinutes = parseInt(curShowTimeSeconds%3600/60);
-		var curSeconds = parseInt(curShowTimeSeconds%60);	
-
-		if (nextSeconds != curSeconds) {
-			
-			if ( parseInt(curHours/10) != parseInt(nextHours/10) ) {
-				addBalls( MARGIN_LEFT + 0, MARGIN_TOP, parseInt(curHours/10));
-			}
-
-			if ( parseInt(curHours%10) != parseInt(nextHours%10) ) {
-				addBalls( MARGIN_LEFT + 15*(RADIUS+1), MARGIN_TOP, parseInt(curHours%10));
-			}
-
-			if ( parseInt(curMinutes/10) != parseInt(nextMinutes/10) ) {
-				addBalls( MARGIN_LEFT + 39*(RADIUS+1), MARGIN_TOP, parseInt(curMinutes/10));
-			}
-
-			if ( parseInt(curMinutes%10) != parseInt(nextMinutes%10) ) {
-				addBalls( MARGIN_LEFT + 54*(RADIUS+1), MARGIN_TOP, parseInt(curMinutes%10));
-			}
-
-			if ( parseInt(curSeconds/10) != parseInt(nextSeconds/10) ) {
-				addBalls( MARGIN_LEFT + 78*(RADIUS+1), MARGIN_TOP, parseInt(curSeconds/10));
-			}									
-
-			if ( parseInt(curSeconds%10) != parseInt(nextSeconds%10) ) {
-				addBalls( MARGIN_LEFT + 93*(RADIUS+1), MARGIN_TOP, parseInt(curSeconds%10));
-			}
-
-			curShowTimeSeconds = nextShowTimeSeconds;
-		}
-
-		// 更新所有球的位置
-		updateBalls();
+		cxt.fill();
 	}
 }
+// 渲染单个数字
+function renderDigit(x, y, num, cxt) {
+	cxt.fillStyle = "rgb(0,102,153)";
+
+	for (var i = 0; i < digit[num].length; i++) {
+		for (var j = 0; j < digit[num][i].length; j++) {
+			if (digit[num][i][j] == 1) {
+				cxt.beginPath();
+				cxt.arc( x+j*2*(RADIUS+1)+(RADIUS+1), y+i*2*(RADIUS+1)+(RADIUS+1), RADIUS, 0, 2*Math.PI );
+				cxt.closePath();
+
+				cxt.fill();
+			}
+		}
+	}
+}
+
+// 更新球的位置，速度等信息
+function updateBalls() {
+	for (var i = 0; i < balls.length; i++) {
+			balls[i].x += balls[i].vx;
+			balls[i].y += balls[i].vy;
+			balls[i].vy += balls[i].g;
+
+			// 下单元碰撞检测
+			if (balls[i].y >= (WINDOW_HEIGHT - RADIUS)) {
+				balls[i].y = WINDOW_HEIGHT - RADIUS;	// 着地
+				balls[i].vy = -balls[i].vy*0.75;	// 增加摩擦系数，速度逐渐降低
+			}
+	}
+
+	var cnt = 0;
+	for (var i = 0; i < balls.length; i++) {
+		if (balls[i].x+RADIUS > 0 && balls[i].x-RADIUS < WINDOW_WIDTH) 
+			balls[cnt++] = balls[i];
+	}
+
+	while(balls.length > cnt) {
+		balls.pop();
+	}
+
+}
+// 为对应的数字添加相应数量的球
+function addBalls(x, y, num) {
+	for (var i = 0; i < digit[num].length; i++) {
+		for (var j = 0; j < digit[num][i].length; j++) {
+			if (digit[num][i][j] == 1) {
+				var aBall = {
+					x: x+j*2*(RADIUS+1)+(RADIUS+1),
+					y: y+i*2*(RADIUS+1)+(RADIUS+1),
+					g: 1.5+Math.random(),
+					vx: Math.pow(-1, Math.ceil(Math.random()*1000))*4,
+					vy: -5,
+					color: colors[Math.floor(Math.random()*colors.length)]
+				}
+				balls.push(aBall);
+			} // end if
+		} // enf for
+	} // end for
+}
+// 更新时间和球的信息
+function update() {
+	var nextShowTimeSeconds = getCurrentShowtimeSeconds();
+	var nextHours = parseInt(nextShowTimeSeconds/3600);
+	var nextMinutes = parseInt(nextShowTimeSeconds%3600/60);
+	var nextSeconds = parseInt(nextShowTimeSeconds%60);	
+
+	var curHours = parseInt(curShowTimeSeconds/3600);
+	var curMinutes = parseInt(curShowTimeSeconds%3600/60);
+	var curSeconds = parseInt(curShowTimeSeconds%60);	
+
+	if (nextSeconds != curSeconds) {
+		
+		if ( parseInt(curHours/10) != parseInt(nextHours/10) ) {
+			addBalls( MARGIN_LEFT + 0, MARGIN_TOP, parseInt(curHours/10));
+		}
+
+		if ( parseInt(curHours%10) != parseInt(nextHours%10) ) {
+			addBalls( MARGIN_LEFT + 15*(RADIUS+1), MARGIN_TOP, parseInt(curHours%10));
+		}
+
+		if ( parseInt(curMinutes/10) != parseInt(nextMinutes/10) ) {
+			addBalls( MARGIN_LEFT + 39*(RADIUS+1), MARGIN_TOP, parseInt(curMinutes/10));
+		}
+
+		if ( parseInt(curMinutes%10) != parseInt(nextMinutes%10) ) {
+			addBalls( MARGIN_LEFT + 54*(RADIUS+1), MARGIN_TOP, parseInt(curMinutes%10));
+		}
+
+		if ( parseInt(curSeconds/10) != parseInt(nextSeconds/10) ) {
+			addBalls( MARGIN_LEFT + 78*(RADIUS+1), MARGIN_TOP, parseInt(curSeconds/10));
+		}									
+
+		if ( parseInt(curSeconds%10) != parseInt(nextSeconds%10) ) {
+			addBalls( MARGIN_LEFT + 93*(RADIUS+1), MARGIN_TOP, parseInt(curSeconds%10));
+		}
+
+		curShowTimeSeconds = nextShowTimeSeconds;
+	}
+
+	// 更新所有球的位置
+	updateBalls();
+}
+
 		
